98800216ac5de49b3c04c4c391e9b9f413ada6c9
Jaesung Kim <jaesung@publy.co>
2023-07-21 20:11:05 +0900

    리액트 네이티브를 위한 컬러 상수 생성 설정

--- style-dictionary.config.js
+++ style-dictionary.config.js
@@ -1,8 +1,15 @@
 const setWith = require('lodash.setwith');
 
+/**
+ * @param {import('style-dictionary/types/TransformedToken').TransformedToken} token
+ */
+function isColor(token) {
+  return token.type === 'color'
+}
+
 /** @type {import('style-dictionary/types/Config').Config} */
 module.exports = {
-  source: ["tokens/global.json"],
+  source: ['tokens/global.json'],
   format: {
     createTailwindTheme: ({ dictionary }) => {
       const theme = {}
@@ -14,15 +21,22 @@ module.exports = {
   },
   platforms: {
     tailwind: {
-      transforms: ["attribute/cti", "name/cti/kebab"],
+      transforms: ['attribute/cti', 'name/cti/kebab'],
       buildPath: 'build/tailwind/global/',
       files: [{
-        filter: (token) => {
-          return token.type === 'color'
-        },
+        filter: isColor,
         destination: 'colors.json',
-        format: "createTailwindTheme"
+        format: 'createTailwindTheme'
       }]
     },
+    rn: {
+      transformGroup: 'react-native',
+      buildPath: './build/react-native/global/',
+      files: [{
+        filter: isColor,
+        destination: 'colors.ts',
+        format: 'javascript/es6'
+      }]
+    }
   }
 }
