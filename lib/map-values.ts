dd525773c2eff0d2fe0200a5bc8ad00a3cfdb123
Jaesung Kim <jaesung@publy.co>
2023-09-04 19:51:46 +0900

    lodash.mapvalues 제거

---
+++ lib/map-values.ts
@@ -0,0 +1,7 @@
+type ObjectIterator<TObject, TResult> = (value: TObject[keyof TObject], key: string) => TResult;
+
+export default function mapValues<T extends object, TResult>(obj: T, callback: ObjectIterator<T, TResult>): { [P in keyof T]: TResult } {
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [key, callback(value, key)])
+  ) as any
+}
