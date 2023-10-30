8eadebb931a19592dcc2fbcfcd8ba57a3f96184c
Jaesung Kim <jaesung@publy.co>
2023-10-30 19:12:46 +0900

    CFVM-131 보더 색 토큰 생성

---
+++ react-native/global/border-colors.ts
@@ -0,0 +1,13 @@
+/**
+ * 직접 수정하지 마세요
+ * Mon Oct 30 2023 19:10:21 GMT+0900 (대한민국 표준시)에 생성됨
+ */
+
+export default {
+  "colorBorderButtonCompleted": "#64748b",
+  "colorBorderButtonDisabled": "#cbd5e1",
+  "colorBorderButtonSecondaryEnabled": "#0F766E",
+  "colorBorderButtonSecondaryHover": "#115E59",
+  "colorBorderButtonPrimaryEnabled": "#334155",
+  "colorBorderButtonPrimaryHover": "#1e293b"
+} as const
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 const colors = require('./tailwind/global/colors.json');
+const borderColors = require('./tailwind/global/border-colors.json');
 const boxShadow = require('./tailwind/global/box-shadows.json');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
     extend: {
-      colors,
+      colors: {
+        ...colors,
+        ...borderColors
+      },
       boxShadow
     }
   },
