d8b19ec7c191f79c9f8563cc545b0c4379f170a9
kimjson <kimjson@users.noreply.github.com>
2023-11-01 09:53:30 +0000

    피그마 토큰으로부터 스타일 딕셔너리 생성

--- react-native/global/border-colors.ts
+++ react-native/global/border-colors.ts
@@ -1,13 +1,13 @@
 /**
  * 직접 수정하지 마세요
- * Mon Oct 30 2023 19:10:21 GMT+0900 (대한민국 표준시)에 생성됨
+ * Wed Nov 01 2023 09:53:29 GMT+0000 (Coordinated Universal Time)에 생성됨
  */
 
 export default {
-  "colorBorderButtonCompleted": "#64748b",
-  "colorBorderButtonDisabled": "#cbd5e1",
-  "colorBorderButtonSecondaryEnabled": "#0F766E",
-  "colorBorderButtonSecondaryHover": "#115E59",
-  "colorBorderButtonPrimaryEnabled": "#334155",
-  "colorBorderButtonPrimaryHover": "#1e293b"
+  "borderColorButtonCompleted": "#64748b",
+  "borderColorButtonDisabled": "#cbd5e1",
+  "borderColorButtonSecondaryEnabled": "#0F766E",
+  "borderColorButtonSecondaryHover": "#115E59",
+  "borderColorButtonPrimaryEnabled": "#334155",
+  "borderColorButtonPrimaryHover": "#1e293b"
 } as const
