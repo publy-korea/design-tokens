6023e5b9f6cc58866508db19063f90456f4015ce
minhyokim <minhyokim@users.noreply.github.com>
2023-11-10 05:41:46 +0000

    피그마 토큰으로부터 스타일 딕셔너리 생성

--- react-native/global/colors.ts
+++ react-native/global/colors.ts
@@ -1,6 +1,6 @@
 /**
  * 직접 수정하지 마세요
- * Tue Nov 07 2023 08:32:57 GMT+0000 (Coordinated Universal Time)에 생성됨
+ * Fri Nov 10 2023 05:41:46 GMT+0000 (Coordinated Universal Time)에 생성됨
  */
 
 export default {
