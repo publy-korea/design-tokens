7fcd984e22850d81cefc57cc0ea4097bfb017ec8
JiwonChaChaCHa <JiwonChaChaCHa@users.noreply.github.com>
2023-11-14 02:13:09 +0000

    피그마 토큰으로부터 스타일 딕셔너리 생성

--- react-native/global/border-colors.ts
+++ react-native/global/border-colors.ts
@@ -1,6 +1,6 @@
 /**
  * 직접 수정하지 마세요
- * Fri Nov 10 2023 05:41:46 GMT+0000 (Coordinated Universal Time)에 생성됨
+ * Tue Nov 14 2023 02:13:09 GMT+0000 (Coordinated Universal Time)에 생성됨
  */
 
 export default {
