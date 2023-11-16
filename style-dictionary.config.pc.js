22991e4bb7529670fc0303d6aca59d0bb734b3ad
Kyungbae Min <kyungbae@publy.co>
2023-11-16 15:40:13 +0900

    web borders tokens

--- style-dictionary.config.pc.js
+++ style-dictionary.config.pc.js
@@ -1,5 +1,5 @@
 const setWith = require('lodash.setwith');
-const {parseToRgb} = require('polished')
+const { parseToRgb } = require('polished');
 
 /**
  * @param {import('style-dictionary/types/TransformedToken').TransformedToken} token
@@ -15,43 +15,92 @@ function isBoxShadow(token) {
   return token.type === 'boxShadow';
 }
 
+/**
+ * @param {import('style-dictionary/types/TransformedToken').TransformedToken} token
+ */
+function isBorder(token) {
+  return token.type === 'border';
+}
+
 /** @type {import('style-dictionary/types/Config').Config} */
 module.exports = {
   source: ['tokens/pc.json'],
   format: {
     createTailwindThemeColor: ({ dictionary }) => {
-      const theme = {}
+      const theme = {};
       dictionary.allTokens.forEach(token => {
-        setWith(theme, token.path.join('.'), `var(--${token.path.join('-')})`, Object);
+        setWith(
+          theme,
+          token.path.join('.'),
+          `var(--${token.path.join('-')})`,
+          Object
+        );
       });
-      
+
       return JSON.stringify(theme, undefined, 2);
     },
     createCSSVariableColor: ({ dictionary }) => {
-      return `.theme-pc {\n  ${dictionary.allTokens.map(token => `--${token.path.join('-')}: ${token.value};`).join('\n  ')}\n}`
+      return `.theme-pc {\n  ${dictionary.allTokens
+        .map(token => `--${token.path.join('-')}: ${token.value};`)
+        .join('\n  ')}\n}`;
+    },
+    createTailwindThemeBorder: ({ dictionary }) => {
+      const theme = {};
+      dictionary.allTokens.forEach(token => {
+        setWith(
+          theme,
+          token.path.join('.'),
+          `var(--${token.path.join('-')})`,
+          Object
+        );
+      });
+
+      return JSON.stringify(theme, undefined, 2);
+    },
+    createCSSVariableBorder: ({ dictionary }) => {
+      return `.theme-pc {\n  ${dictionary.allTokens
+        .map(
+          token =>
+            `--${token.path.join('-')}: ${token.value.style} ${
+              token.value.width
+            }px ${token.value.color};`
+        )
+        .join('\n  ')}\n}`;
     },
     createTailwindThemeBoxShadow: ({ dictionary }) => {
-      const theme = {}
+      const theme = {};
       dictionary.allTokens.forEach(token => {
-        const {x, y, blur, spread} = token.value;
+        const { x, y, blur, spread } = token.value;
         const [color, alpha] = token.value.color.split(', ');
-        const {red, green, blue} = parseToRgb(color);
-        setWith(theme, token.path.join('-'), `${x}px ${y}px ${blur}px ${spread}px rgb(${red} ${green} ${blue} / ${alpha})`, Object);
+        const { red, green, blue } = parseToRgb(color);
+        setWith(
+          theme,
+          token.path.join('-'),
+          `${x}px ${y}px ${blur}px ${spread}px rgb(${red} ${green} ${blue} / ${alpha})`,
+          Object
+        );
       });
       return JSON.stringify(theme, undefined, 2);
     },
-    createReactNativeTheme: ({dictionary}) => {
-      const theme = {}
+    createReactNativeTheme: ({ dictionary }) => {
+      const theme = {};
       dictionary.allTokens.forEach(token => {
         const [first, ...rest] = token.path;
-        const keys = [first, ...rest.map(key => key.charAt(0).toUpperCase() + key.slice(1))];
+        const keys = [
+          first,
+          ...rest.map(key => key.charAt(0).toUpperCase() + key.slice(1)),
+        ];
         theme[keys.join('')] = token.value;
       });
 
-      const header = `/**\n * 직접 수정하지 마세요\n * ${new Date()}에 생성됨\n */`
+      const header = `/**\n * 직접 수정하지 마세요\n * ${new Date()}에 생성됨\n */`;
 
-      return `${header}\n\nexport default ${JSON.stringify(theme, undefined, 2)} as const`;
-    }
+      return `${header}\n\nexport default ${JSON.stringify(
+        theme,
+        undefined,
+        2
+      )} as const`;
+    },
   },
   platforms: {
     css: {
@@ -61,9 +110,14 @@ module.exports = {
         {
           filter: isColor,
           destination: 'pc/colors.css',
-          format: 'createCSSVariableColor'
+          format: 'createCSSVariableColor',
         },
-      ]
+        {
+          filter: isBorder,
+          destination: 'pc/borders.css',
+          format: 'createCSSVariableBorder',
+        },
+      ],
     },
     tailwind: {
       transforms: ['attribute/cti', 'name/cti/kebab'],
@@ -72,9 +126,14 @@ module.exports = {
         {
           filter: isColor,
           destination: 'colors.json',
-          format: 'createTailwindThemeColor'
+          format: 'createTailwindThemeColor',
         },
-      ]
+        {
+          filter: isBorder,
+          destination: 'borders.json',
+          format: 'createTailwindThemeBorder',
+        },
+      ],
     },
-  }
-}
+  },
+};
