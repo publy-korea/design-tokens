d24c759e67b104a4d45e59860d3cb33148761c0c
Kyungbae Min <kyungbae@publy.co>
2023-11-20 11:15:15 +0900

    helpers 분리

---
+++ helpers.js
@@ -0,0 +1,109 @@
+const setWith = require('lodash.setwith');
+const { parseToRgb } = require('polished');
+
+module.exports = {
+  filters:{
+  	/**
+  	 * @param {import('style-dictionary/types/TransformedToken').TransformedToken} token
+  	 */
+  	isColor: token => {
+  		return token.type === "color";
+  	},
+  	/**
+  	 * @param {import('style-dictionary/types/TransformedToken').TransformedToken} token
+  	 */
+  	isBoxShadow: token => {
+  		return token.type === "boxShadow";
+  	},
+  	/**
+  	 * @param {import('style-dictionary/types/TransformedToken').TransformedToken} token
+  	 */
+  	isBorder: token => {
+  		return token.type === "border";
+  	},
+  },
+  tailwindThemeFormatter: {
+  	createTailwindThemeColor: ({ dictionary }) => {
+  		const theme = {};
+  		dictionary.allTokens.forEach(token => {
+  			setWith(
+  				theme,
+  				token.path.join("."),
+  				`var(--${token.path.join("-")})`,
+  				Object
+  			);
+  		});
+
+  		return JSON.stringify(theme, undefined, 2);
+  	},
+    createTailwindThemeBorder: ({ dictionary }) => {
+  		const theme = {};
+  		dictionary.allTokens.forEach(token => {
+  			setWith(
+  				theme,
+  				token.path.join("."),
+  				`var(--${token.path.join("-")})`,
+  				Object
+  			);
+  		});
+
+  		return JSON.stringify(theme, undefined, 2);
+  	},
+    createTailwindThemeBoxShadow: ({ dictionary }) => {
+  		const theme = {};
+  		dictionary.allTokens.forEach(token => {
+  			const { x, y, blur, spread } = token.value;
+  			const [color, alpha] = token.value.color.split(", ");
+  			const { red, green, blue } = parseToRgb(color);
+  			setWith(
+  				theme,
+  				token.path.join("-"),
+  				`${x}px ${y}px ${blur}px ${spread}px rgb(${red} ${green} ${blue} / ${alpha})`,
+  				Object
+  			);
+  		});
+  		return JSON.stringify(theme, undefined, 2);
+  	},
+  },
+  cssVariableFormatter: mode => ({
+  	createCSSVariableColor:
+  		({ dictionary }) => {
+  			return `.theme-${mode} {\n  ${dictionary.allTokens
+  				.map(token => `--${token.path.join("-")}: ${token.value};`)
+  				.join("\n  ")}\n}`;
+  		},
+  	
+  	createCSSVariableBorder:
+      ({ dictionary }) => {
+    		return `.theme-${mode} {\n  ${dictionary.allTokens
+    			.map(
+    				token => {
+              const { style, width, color } = token.value;
+    					return `--${token.path.join("-")}: ${style} ${width}px ${color};`
+            }
+    			)
+    			.join("\n  ")}\n}`;
+  	},
+  }),
+  reactNativeThemeFormatter: {
+  	createReactNativeTheme: ({ dictionary }) => {
+  		const theme = {};
+  		dictionary.allTokens.forEach(token => {
+  			const [first, ...rest] = token.path;
+  			const keys = [
+  				first,
+  				...rest.map(key => key.charAt(0).toUpperCase() + key.slice(1)),
+  			];
+  			theme[keys.join("")] = token.value;
+  		});
+
+  		const header = `/**\n * 직접 수정하지 마세요\n * ${new Date()}에 생성됨\n */`;
+
+  		return `${header}\n\nexport default ${JSON.stringify(
+  			theme,
+  			undefined,
+  			2
+  		)} as const`;
+  	},
+  },
+};
