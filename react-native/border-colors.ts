21e3d1f98d76d580a1250e5cfd6e040d5deeb742
kimjson <kimjson@users.noreply.github.com>
2024-01-04 09:47:42 +0000

    피그마 토큰으로부터 스타일 딕셔너리 생성

--- react-native/border-colors.ts
+++ react-native/border-colors.ts
@@ -1,6 +1,6 @@
 /**
  * 직접 수정하지 마세요
- * Thu Jan 04 2024 09:44:46 GMT+0000 (Coordinated Universal Time)에 생성됨
+ * Thu Jan 04 2024 09:47:41 GMT+0000 (Coordinated Universal Time)에 생성됨
  */
 
 export default {
